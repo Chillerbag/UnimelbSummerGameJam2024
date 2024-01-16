93da652d385f372bbc5eda7ad8774d656211f288
Luke Bosnic <lbos0003@student.monash.edu>
2024-01-16 18:38:02 +1100

    Added a Timer
    The timer will just keep going up forever.
    It's controlled by EventManager, and the script Game Manager is used to start the event which controls the timer.
    Game Manager will be used to control days passing in future.

---
+++ My project/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90f444fcf0687e7c6eeb1e4a29748e5c5220e5ca687a826624b62216b87d54cd
+size 341
---
+++ My project/Assets/Scripts/Timer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4d2bdc82c1836c2789a4ac35f9a56cc5ead811bea17df7bf7799922ba9d9bac
+size 1590
