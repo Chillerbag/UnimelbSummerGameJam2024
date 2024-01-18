62ea458d92ebbd3e2900dca0e48661d0ead64695
computergames <ethanwhawkins@gmail.com>
2024-01-19 02:15:20 +1100

    Finished game logic and all gameplay files in json and videos!
    lots to debug. Videoloader relies on a heavy script that causes strange issues. Not sure source of other issues

--- My project/Assets/Scripts/SpeedrunFactory.cs
+++ My project/Assets/Scripts/SpeedrunFactory.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49d4c41d3f8f70747078e36e2f00352a497f99e708724c42d624e480ad031017
-size 6238
+oid sha256:f58b9e103f84a330f77df0dbcc9f2f78f75e509a9d64fa408e006ffe324c097b
+size 6450
