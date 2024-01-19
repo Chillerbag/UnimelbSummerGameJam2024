cc1d1eada1ac9f2958899d5a97b1f07ae03342e8
Luke Bosnic <lbos0003@student.monash.edu>
2024-01-20 10:23:05 +1100

    added main menu

--- My project/Assets/Scripts/Timer.cs
+++ My project/Assets/Scripts/Timer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4358c64e92efe4c3cd472f06effb86bfe2b6b99bf018a58f2d196e3322906686
-size 1971
+oid sha256:ce645ee727e554ba0f2bfe70dc9523e15f24a3ca8073e4d47ece8b08f97ba61b
+size 1963
